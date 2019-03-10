acba357bf8dc18b44a6b85ef4dd042698e8125ac
Alex Cho <alexsjcho@gmail.com>
2019-03-09 21:08:02 -0800

    finished maxMultiples problem

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,7 @@
-function maxMultiple(divisor: number, bound: number): number  {
+function maxMultiple(divisor: number, bound: number): number {
+  let remainder = bound % divisor;
 
+  return bound - remainder;
 }
 
-console.log(maxMultiple(3,10));
+console.log(maxMultiple(3, 10));
