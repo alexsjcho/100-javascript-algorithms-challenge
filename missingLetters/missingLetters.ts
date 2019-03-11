b4710795ddbb30603db2dbfb3f65aaf83bd4f110
Alex Cho <alexsjcho@gmail.com>
2019-03-10 21:24:11 -0700

    finished newNumeralSystem problem

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,5 +1,15 @@
 function missingLetters(str: string): string {
-   
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const chars = str.split("");
+  if (chars[0] !== "a") {
+    return undefined;
+  }
+
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] !== alphabet[i]) {
+      return alphabet[i];
+    }
+  }
 }
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
